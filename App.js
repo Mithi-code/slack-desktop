257fa6fb50c3dd06fa9c0fe13f054119ba00edca
Mithlesh kumar <mithlesh.9m@gmail.com>
2023-07-17 21:18:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { styled } from "styled-components";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from "./components/Login";
+
+function App() {
+  const [user, loading] = useAuthState(auth);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <AppBody>
+              <Sidebar />
+              <Routes>
+                <Route path="/" element={<Chat />}></Route>
+              </Routes>
+            </AppBody>
+          </>
+        )}
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
